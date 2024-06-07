b6def3961b95b5523ed385ef5898b592a09f61df
nomanluhar <luharnoman409@gmail.com>
2024-06-07 22:20:17 +0530

    work

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 const inputRoute = require("./routes/form-input");
-app.use("/", inputRoute);
+app.use("/", inputRoute); 
 
 const PORT = 3000;
 app.listen(PORT, () => {
